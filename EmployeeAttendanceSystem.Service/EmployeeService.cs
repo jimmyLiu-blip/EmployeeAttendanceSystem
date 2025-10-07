46be7136d1493f712d8410ab5bbaa197cd7332ae
APAC\Jimmy-HY_Liu <jimmy-hy.liu@sgs.com>
2025-10-07 17:01:59 +0800

    調整Service

--- EmployeeAttendanceSystem.Service/EmployeeService.cs
+++ EmployeeAttendanceSystem.Service/EmployeeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EmployeeAttendanceSystem.Domain;
+using EmployeeAttendanceSystem.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,24 @@ namespace EmployeeAttendanceSystem.Service
 {
     public class EmployeeService
     {
+        private readonly IEmployeeRepository _employeeRepository;
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public void AddEmployee(string name, string department, string jobTitle)
+        {
+            var newEmployee = new Employee { Name = name, Department = department, JobTitle = jobTitle };
+
+            _employeeRepository.AddEmployee(newEmployee);
+        }
+        public Dictionary<int, string> GetAllEmployeeNames()
+        { 
+            return _employeeRepository.GetAllEmployeeNames();
+        }
+        public Employee GetAllEmployees()
+        {
+            return _employeeRepository.GetAllEmployees();
+        }
     }
 }
---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE AttendanceRecord (
+    RecordId INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeId INT NOT NULL,
+    Date DATE NOT NULL,
+    CheckIn DATETIME NULL,
+    CheckOut DATETIME NULL,
+    WorkHours FLOAT NULL,
+    IsLate BIT DEFAULT 0,
+    IsEarlyLeave BIT DEFAULT 0,
+    FOREIGN KEY (EmployeeId) REFERENCES Employee(EmployeeId) ON DELETE CASCADE
+);
+GO
